48d7b739a532b672dc57c6377f5228ad0a4880a6
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-06-03 13:38:43 -0500

    Add files via upload

---
+++ src/Common/PulseEffectAction.cpp
@@ -0,0 +1,15 @@
+
+#include "includes.h"
+
+
+bool PulseEffectAction::isFinished()
+{
+    return;
+}
+
+
+
+/* Unknown Return: PulseEffectAction::step(float p0){}; */
+
+
+/* Unknown Return: PulseEffectAction::valueForDelta(float p0, float p1, float p2, float p3){}; */
