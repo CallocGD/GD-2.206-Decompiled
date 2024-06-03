5423d17488de640114521125cd4ae62ae50ccfe5
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-06-03 13:43:27 -0500

    Upload Layers 1/2

---
+++ src/Layers/AccountHelpLayer.cpp
@@ -0,0 +1,60 @@
+
+#include "includes.h"
+
+
+void AccountHelpLayer::FLAlert_Clicked(FLAlertLayer* p0, bool p1)
+{
+    return;
+}
+
+
+
+/* Unknown Return: AccountHelpLayer::accountStatusChanged(){}; */
+
+AccountHelpLayer* AccountHelpLayer::create()
+{
+    return;
+}
+
+
+void AccountHelpLayer::customSetup()
+{
+    return;
+}
+
+
+
+/* Unknown Return: AccountHelpLayer::doUnlink(){}; */
+
+
+/* Unknown Return: AccountHelpLayer::exitLayer(){}; */
+
+void AccountHelpLayer::layerHidden()
+{
+    return;
+}
+
+
+void AccountHelpLayer::onAccountManagement(cocos2d::CCObject* sender)
+{
+    return;
+}
+
+
+void AccountHelpLayer::onReLogin(cocos2d::CCObject* sender)
+{
+    return;
+}
+
+
+void AccountHelpLayer::onUnlink(cocos2d::CCObject* sender)
+{
+    return;
+}
+
+
+
+/* Unknown Return: AccountHelpLayer::updatePage(){}; */
+
+
+/* Unknown Return: AccountHelpLayer::verifyUnlink(){}; */
