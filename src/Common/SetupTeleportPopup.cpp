6815adc099ff9772b601002cf01f8960e0d50d15
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-06-03 13:40:33 -0500

    Final Batch of Common

---
+++ src/Common/SetupTeleportPopup.cpp
@@ -0,0 +1,39 @@
+
+#include "includes.h"
+
+
+SetupTeleportPopup* SetupTeleportPopup::create(TeleportPortalObject* p0, cocos2d::CCArray* p1, int p2, bool p3)
+{
+    return;
+}
+
+
+
+/* Unknown Return: SetupTeleportPopup::determineStartValues(){}; */
+
+bool SetupTeleportPopup::init(TeleportPortalObject* p0, cocos2d::CCArray* p1, int p2, bool p3)
+{
+    return;
+}
+
+
+void SetupTeleportPopup::onTeleportGravity(cocos2d::CCObject* sender)
+{
+    return;
+}
+
+
+void SetupTeleportPopup::updateDefaultTriggerValues()
+{
+    return;
+}
+
+
+
+/* Unknown Return: SetupTeleportPopup::updateTeleportGravityState(int p0){}; */
+
+void SetupTeleportPopup::valueDidChange(int p0, float p1)
+{
+    return;
+}
+
