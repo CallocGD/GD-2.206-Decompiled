175984bf5e8b40d8a76d73dfa0261d28271bc8d4
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-10-19 21:31:17 -0500

    Document GJHttpResult and functions

--- headers/Common/GJHttpResult.h
+++ headers/Common/GJHttpResult.h
@@ -4,10 +4,27 @@
 
 #include "../includes.h"
 
+/// @brief Used primarly as a Carrier Node to prevent Http Items from deallocating during the initial response time
 class GJHttpResult: public cocos2d::CCNode {
 public:
-    static GJHttpResult* create(bool p0, std::string p1, std::string p2, GJHttpType p3);
-    bool init(bool p0, std::string p1, std::string p2, GJHttpType p3);
+    /// @brief Creates a Node for carrying response items
+    /// @param success weather or not the http request succeeded
+    /// @param response The Response data of the result
+    /// @param tag the Download Tag of the request
+    /// @param httptype The Type of http request that was made
+    static GJHttpResult* create(bool success, std::string response, std::string tag, GJHttpType httptype);
+    
+    /// @brief initalizes a GJHttpResult for carrying response items
+    /// @param success weather or not the http request succeeded
+    /// @param response The Response data of the result
+    /// @param tag the Download Tag of the request
+    /// @param httptype The Type of http request that was made
+    bool init(bool success, std::string response, std::string tag, GJHttpType httptype);
+
+    bool success;
+    std::string response;
+    std::string tag;
+    GJHttpType httptype;
 };
 
 #endif /* __GJHTTPRESULT_H__ */
