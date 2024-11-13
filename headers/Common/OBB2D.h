34691a9fb71f42d4b88f6cb9e9bee3c5e73dc012
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-11-12 18:47:28 -0600

    Update OBB2D.h

--- headers/Common/OBB2D.h
+++ headers/Common/OBB2D.h
@@ -6,14 +6,22 @@
 
 class OBB2D: public cocos2d::CCNode {
 public:
-    static OBB2D* create(cocos2d::CCPoint p0, float p1, float p2, float p3);
-    TodoReturn calculateWithCenter(cocos2d::CCPoint p0, float p1, float p2, float p3);
-    TodoReturn computeAxes();
-    TodoReturn getBoundingRect();
-    bool init(cocos2d::CCPoint p0, float p1, float p2, float p3);
-    TodoReturn orderCorners();
-    TodoReturn overlaps(OBB2D* p0);
-    TodoReturn overlaps1Way(OBB2D* p0);
+    static OBB2D* create(cocos2d::CCPoint center, float width,float height,float rotationAngle);
+    void calculateWithCenter(cocos2d::CCPoint center, float width,float height,float rotationAngle);
+    void computeAxes();
+    cocos2d::CCRect getBoundingRect();
+    bool init(cocos2d::CCPoint center, float width,float height,float rotationAngle);
+    void orderCorners();
+    bool overlaps(OBB2D* other);
+    bool overlaps1Way(OBB2D* other);
+
+    cocos2d::CCPoint m_corners[4]; /* 0x108 */
+    cocos2d::CCPoint m_positions[4]; /* 0x128 */
+    cocos2d::CCPoint m_horizontalDifference;
+    cocos2d::CCPoint m_verticalDifference;
+    cocos2d::CCPoint m_axes[4];
+    cocos2d::CCPoint m_center;
 };
+
 
 #endif /* __OBB2D_H__ */
