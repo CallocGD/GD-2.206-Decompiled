e8159403c7adc9a3110ad5ffd4c645e785d5c3b4
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-11-13 10:50:36 -0600

    Update CCSpriteCOpacity.h

--- headers/CustomCCClasses/CCSpriteCOpacity.h
+++ headers/CustomCCClasses/CCSpriteCOpacity.h
@@ -6,9 +6,9 @@
 
 class CCSpriteCOpacity: public cocos2d::CCSprite {
 public:
-    TodoReturn createWithSpriteFrame(cocos2d::CCSpriteFrame* p0);
-    TodoReturn createWithSpriteFrameName(char const* p0);
-    virtual void setOpacity(unsigned char p0);
+    TodoReturn createWithSpriteFrame(cocos2d::CCSpriteFrame* frame);
+    TodoReturn createWithSpriteFrameName(char const* frameName);
+    virtual void setOpacity(unsigned char opacity);
 };
 
 #endif /* __CCSPRITECOPACITY_H__ */
