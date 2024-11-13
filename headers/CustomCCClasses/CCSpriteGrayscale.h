f908a30e95d0219388eeb45a62e6d897234ea29d
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-11-12 20:03:08 -0600

    Update CCSpriteGrayscale.h

--- headers/CustomCCClasses/CCSpriteGrayscale.h
+++ headers/CustomCCClasses/CCSpriteGrayscale.h
@@ -11,14 +11,15 @@ class CCSpriteWithHue;
 
 class CCSpriteGrayscale: public CCSpriteWithHue {
 public:
-    static CCSpriteGrayscale* create(std::string const& p0, cocos2d::CCRect const& p1);
-    static CCSpriteGrayscale* create(std::string const& p0);
-    static CCSpriteGrayscale* createWithSpriteFrame(cocos2d::CCSpriteFrame* p0);
-    static CCSpriteGrayscale* createWithSpriteFrameName(std::string const& p0);
-    TodoReturn createWithTexture(cocos2d::CCTexture2D* p0, cocos2d::CCRect const& p1, bool p2);
-    TodoReturn createWithTexture(cocos2d::CCTexture2D* p0);
+    static CCSpriteGrayscale* create(std::string const& file, cocos2d::CCRect const& rect);
+    static CCSpriteGrayscale* create(std::string const& file);
+    static CCSpriteGrayscale* createWithSpriteFrame(cocos2d::CCSpriteFrame* frame);
+    static CCSpriteGrayscale* createWithSpriteFrameName(std::string const& frameName);
+    static CCSpriteGrayscale* createWithTexture(cocos2d::CCTexture2D* texture, cocos2d::CCRect const& rect, bool rotated);
+    static CCSpriteGrayscale* createWithTexture(cocos2d::CCTexture2D* texture);
     virtual std::string getShaderName();
     virtual const char* shaderBody();
 };
+
 
 #endif /* __CCSPRITEGRAYSCALE_H__ */
