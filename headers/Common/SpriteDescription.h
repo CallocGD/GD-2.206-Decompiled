11414a6c1b4ec9f0913b199f1ea192d95ae49fef
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-11-13 23:39:10 -0600

    Update SpriteDescription.h

--- headers/Common/SpriteDescription.h
+++ headers/Common/SpriteDescription.h
@@ -4,12 +4,37 @@
 
 #include "../includes.h"
 
-class SpriteDescription: public cocos2d::CCObject {
+class SpriteDescription : cocos2d::CCObject {
 public:
-    TodoReturn createDescription(cocos2d::CCDictionary* p0);
-    TodoReturn createDescription(DS_Dictionary* p0);
-    TodoReturn initDescription(cocos2d::CCDictionary* p0);
-    TodoReturn initDescription(DS_Dictionary* p0);
+    // virtual ~SpriteDescription();
+    SpriteDescription() {
+        m_position = CCPointMake(0.f, 0.f);
+        m_scale = CCPointMake(0.f, 0.f);
+        m_flipped = CCPointMake(0.f, 0.f);
+        m_rotation = 0;
+        m_zValue = 0;
+        m_tag = 0;
+        m_usesCustomTag = false;
+        m_texture = nullptr;
+    }
+
+    static SpriteDescription* createDescription(cocos2d::CCDictionary* dict);
+
+    static SpriteDescription* createDescription(DS_Dictionary* dict);
+
+    
+    bool initDescription(cocos2d::CCDictionary* dict);
+
+    bool initDescription(DS_Dictionary* dict);
+
+    cocos2d::CCPoint m_position;
+    cocos2d::CCPoint m_scale;
+    cocos2d::CCPoint m_flipped;
+    float m_rotation;
+    int m_zValue;
+    int m_tag;
+    bool m_usesCustomTag;
+    cocos2d::CCSpriteFrame* m_texture;
 };
 
 #endif /* __SPRITEDESCRIPTION_H__ */
