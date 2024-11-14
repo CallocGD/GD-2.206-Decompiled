8337ce75f4eb57922c036644ec89b9d8314a293a
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-11-13 23:59:28 -0600

    Create TOSPopup.cpp

---
+++ src/Common/TOSPopup.cpp
@@ -0,0 +1,43 @@
+#include "includes.h"
+
+
+TOSPopup* TOSPopup::create()
+{
+    TOSPopup* tos = new TOSPopup;
+    if (tos->init()){
+        tos->autorelease();
+        return tos;
+    }
+    CC_SAFE_DELETE(tos);
+    return nullptr;
+}
+
+
+/* TODO: Bigass Function */
+
+PASS(bool TOSPopup::init(), "Bigass Function, will do this later...")
+
+
+
+NOOP(void TOSPopup::keyBackClicked());
+
+
+
+void TOSPopup::onClose(cocos2d::CCObject* sender)
+{
+    GameManager::sharedState()->setUGV("30",true);
+    setKeypadEnabled(false);
+    removeFromParentAndCleanup(true);
+}
+
+
+void TOSPopup::onPrivacy(cocos2d::CCObject* sender)
+{
+    cocos2d::CCApplication::sharedApplication()->openURL("https://www.robtopgames.com/privacy");
+}
+
+
+void TOSPopup::onTOS(cocos2d::CCObject* sender)
+{
+    cocos2d::CCApplication::sharedApplication()->openURL("https://www.robtopgames.com/tos");
+}
