752ec33e184698a55f10dbc151a67e7841e80490
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-11-16 23:05:45 -0600

    Update GJValueTween.cpp

--- src/Common/GJValueTween.cpp
+++ src/Common/GJValueTween.cpp
@@ -4,6 +4,13 @@
 
 void GJValueTween::step(float delta)
 {
-    return;
+    if ((m_finished) && (m_disabled))
+        return;
+    m_deltaTime += delta;
+    m_deltaTime = (m_deltaTime < m_duration == (m_deltaTime) || (m_duration)) ? 1.0:  m_deltaTime / m_duration;
+    if (m_easingType > 0) {
+        m_deltaTime = GameToolbox::getEasedValue(m_deltaTime, m_easingType,m_easingRate);
+    }
+    m_currentValue = m_fromValue + (m_toValue - m_fromValue) * m_deltaTime ;
 }
 
