580829e8e1507a8022c55e1c84e4c221afea5104
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-11-30 00:19:31 -0600

    Update GameToolbox.h

--- headers/Tools/GameToolbox.h
+++ headers/Tools/GameToolbox.h
@@ -4,6 +4,9 @@
 
 #include "../includes.h"
 
+/* Rumor has it that SMJS was given a copy of this from robtop but is not allowed to show it to the public, 
+ * Lol, I'm gonna reverse engineer it all soon :Lenny: */
+
 class GameToolbox {
 public:
     static void addBackButton(cocos2d::CCLayer* p0, cocos2d::CCMenuItem* p1);
@@ -26,7 +29,7 @@ public:
     static TodoReturn getDropActionWDelay(float p0, float p1, float p2, cocos2d::CCNode* p3, cocos2d::SEL_CallFunc p4);
     static TodoReturn getDropActionWEnd(float p0, float p1, float p2, cocos2d::CCAction* p3, float p4);
     static TodoReturn getEasedAction(cocos2d::CCActionInterval* p0, int p1, float p2);
-    static TodoReturn getEasedValue(float p0, int p1, float p2);
+    static float getEasedValue(float p0, int p1, float p2);
     static TodoReturn getfast_srand();
     static TodoReturn getInvertedEasing(int p0);
     static TodoReturn getLargestMergedIntDicts(cocos2d::CCDictionary* p0, cocos2d::CCDictionary* p1);
@@ -36,7 +39,8 @@ public:
     static gd::string getTimeString(int p0, bool p1);
     static TodoReturn hsvFromString(std::string const& p0, char const* p1);
     static TodoReturn intToShortString(int p0);
-    static TodoReturn intToString(int p0);
+    /* NOTE: This function is inlined... */
+    static std::string intToString(int p0);
     static bool isIOS();
     static bool isRateEasing(int p0);
     static TodoReturn mergeDictsSaveLargestInt(cocos2d::CCDictionary* p0, cocos2d::CCDictionary* p1);
@@ -46,8 +50,8 @@ public:
     static TodoReturn openAppPage();
     static TodoReturn openRateURL(std::string p0, std::string p1);
     static TodoReturn particleFromString(std::string const& p0, cocos2d::CCParticleSystemQuad* p1, bool p2);
-    static TodoReturn particleFromStruct(cocos2d::ParticleStruct const& p0, cocos2d::CCParticleSystemQuad* p1, bool p2);
-    static TodoReturn particleStringToStruct(std::string const& p0, cocos2d::ParticleStruct& p1);
+    // static TodoReturn particleFromStruct(cocos2d::ParticleStruct const& p0, cocos2d::CCParticleSystemQuad* p1, bool p2);
+    // static TodoReturn particleStringToStruct(std::string const& p0, cocos2d::ParticleStruct& p1);
     static TodoReturn pointsToString(int p0);
     static TodoReturn postClipVisit();
     static TodoReturn preVisitWithClippingRect(cocos2d::CCNode* p0, cocos2d::CCRect p1);
@@ -59,8 +63,9 @@ public:
     static TodoReturn stringSetupToMap(std::string const& p0, char const* p1, std::map<std::string, std::string>& p2);
     static TodoReturn strongColor(cocos2d::ccColor3B p0);
     static gd::string timestampToHumanReadable(time_t p0, time_t p1);
-    static TodoReturn transformColor(cocos2d::ccColor3B const& p0, cocos2d::ccHSVValue p1);
-    static TodoReturn transformColor(cocos2d::ccColor3B const& p0, float p1, float p2, float p3);
+    static cocos2d::ccColor3B transformColor(cocos2d::ccColor3B const& p0, cocos2d::ccHSVValue p1);
+    static cocos2d::ccColor3B transformColor(cocos2d::ccColor3B const& p0, float p1, float p2, float p3);
 };
 
+#endif /* __GAMETOOLBOX_H__ */
 #endif /* __GAMETOOLBOX_H__ */
