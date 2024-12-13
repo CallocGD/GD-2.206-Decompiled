2eb92bb20bb56431eea32a032f1ccc3154578a8b
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-12-13 14:22:58 -0600

    Update CCPartAnimSprite.h

--- headers/CustomCCClasses/CCPartAnimSprite.h
+++ headers/CustomCCClasses/CCPartAnimSprite.h
@@ -6,26 +6,35 @@
 
 class CCPartAnimSprite: public cocos2d::CCSprite {
 public:
-    TodoReturn changeTextureOfID(char const* p0, char const* p1);
-    TodoReturn countParts();
-    TodoReturn createWithAnimDesc(char const* p0, cocos2d::CCTexture2D* p1, bool p2);
-    TodoReturn dirtify();
-    TodoReturn getSpriteForKey(char const* p0);
-    TodoReturn initWithAnimDesc(char const* p0, cocos2d::CCTexture2D* p1, bool p2);
+    void changeTextureOfID(char *textureFrame, char *displayFrame);
+    unsigned int countParts();
+    static CCPartAnimSprite* createWithAnimDesc(char const* frame, cocos2d::CCTexture2D* texture, bool rotated);
+    void dirtify();
+    CCSpritePart* getSpriteForKey(char const* partID);
+    bool initWithAnimDesc(char const* frame, cocos2d::CCTexture2D* texture, bool rotated);
     void setColor(cocos2d::ccColor3B p0);
     void setFlipX(bool p0);
     void setFlipY(bool p0);
-    TodoReturn transformSprite(SpriteDescription* p0);
-    TodoReturn tweenSpriteTo(SpriteDescription* p0, float p1);
-    TodoReturn tweenToFrame(cocos2d::CCSpriteFrame* p0, float p1);
-    virtual void setScaleX(float p0);
-    virtual void setScaleY(float p0);
-    virtual void setScale(float p0);
-    virtual void setOpacity(unsigned char p0);
-    virtual void setBlendFunc(cocos2d::_ccBlendFunc p0);
-    virtual void setDisplayFrame(cocos2d::CCSpriteFrame* p0);
-    virtual bool isFrameDisplayed(cocos2d::CCSpriteFrame* p0);
+    void transformSprite(SpriteDescription* desc);
+    void tweenSpriteTo(SpriteDescription* desc, float duration);
+    void tweenToFrame(cocos2d::CCSpriteFrame* frame, float duration);
+    virtual void setScaleX(float scaleX);
+    virtual void setScaleY(float scaleY);
+    virtual void setScale(float scale);
+    virtual void setOpacity(unsigned char opacity);
+    virtual void setBlendFunc(cocos2d::ccBlendFunc func);
+    virtual void setDisplayFrame(cocos2d::CCSpriteFrame* frame);
+    virtual bool isFrameDisplayed(cocos2d::CCSpriteFrame* frame);
+
+
     virtual cocos2d::CCSpriteFrame* displayFrame();
+
+    cocos2d::CCDictionary* m_spritePartIDs;
+    cocos2d::CCSpriteFrame* m_spriteFrame;
+    bool m_hasChanged;
+    cocos2d::CCSize m_unkSize;
+    cocos2d::CCArray* m_spriteParts;
+
 };
 
 #endif /* __CCPARTANIMSPRITE_H__ */
